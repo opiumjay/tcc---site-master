07d8ad05190a18017188ab28b55a46941ef408b7
Romulo-Moraes <romuloperes6@gmail.com>
2023-05-03 22:36:57 -0300

    ajeitando

--- html/acervo.php
+++ html/acervo.php
@@ -52,8 +52,8 @@
         }
     </script>
 
-     <!--! navbar -->
-     <nav class="navbar navbar-dark" style="background-color: #000000;">
+    <!--! navbar -->
+    <nav class="navbar navbar-dark" style="background-color: #000000;">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.html">
                 <img src="../img/navbar/back.png" alt="Bootstrap" width="25" height="25">
@@ -121,276 +121,146 @@
         </div>
     </nav>
 
-     <!--div principal-->
-     <div style="margin-top: 5%; padding: 2%;">
+    <!--div principal-->
+    <div style="margin-top: 5%; padding: 2%;">
 
-<!-- Modal -->
-<div class="modal fade" id="filtroacervo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Filtro</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-
-                <label>Categoria</label>
-
-                <select class="selectpicker" multiple data-live-search="true">
-                    <option>Top Wear</option>
-                    <option>Calças</option>
-                    <option>Tênis</option>
-                    <option>Acessórios</option>
-                    <option>Head Wear</option>
-                </select>
-                <br>
-                <label>Formato</label>
-                <select class="selectpicker" multiple data-live-search="true">
-                    <option>Justo</option>
-                    <option>Normal</option>
-                    <option>Largo</option>
-                </select>
-                <br>
-                <div class="d-inline-flex p-2">
-                    <label>Cor:</label>
-                    <div style="margin-left: 15%;">
-                        <input type="color" name="corFiltro">
+        <!-- Modal -->
+        <div class="modal fade" id="filtroacervo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Filtro</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+                    <form action="filtroA.php" method="post" enctype="multipart/form-data">
+                        <div class="modal-body">
+
+                            <label>Categoria</label>
+
+                            <select class="selectpicker" multiple data-live-search="true" name="">
+                                <option value="">Top Wear</option>
+                                <option>Calças</option>
+                                <option>Tênis</option>
+                                <option>Acessórios</option>
+                                <option>Head Wear</option>
+                            </select>
+                            <br>
+                            <label>Formato</label>
+                            <select class="selectpicker" multiple data-live-search="true">
+                                <option>Justo</option>
+                                <option>Normal</option>
+                                <option>Largo</option>
+                            </select>
+                            <br>
+                            <div class="d-inline-flex p-2">
+                                <label>Cor:</label>
+                                <div style="margin-left: 15%;">
+                                    <input type="color" name="corFiltro">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-secondary">Aplicar Filtros</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-secondary">Aplicar Filtros</button>
-            </div>
         </div>
-    </div>
-</div>
 
-<!-- div que porta todo o grid-->
-<div class="grid-container">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-    <img src="../img/teste.jpeg">
-</div>
-</div>
-
-<!--! botão add peça -->
-<div class="circle" onclick="showPopup()">
-        <div class="cross">
-        </div>
-    </div>
-
-    <!--! pop up add peça -->
-    <div id="popup" class="popup">
-        <div class="popup-content">
-            <form>
-                <select id="selecaoDeCategoria" name="selecaoDeCategoria">
-                    <option selected="nullCategoria">Categoria</option>
-                    <optgroup label="Vestuário Superior">
-                        <option value="camisa">Camiseta</option>
-                        <option value="moletom">Moletom</option>
-                        <option value="jaqueta">Jaqueta</option>
-                        <option value="regata">Regata</option>
-                        <option value="manga_longa">Camisa de Manga Longa</option>
-                        <option value="sueter">Suéter</option>
-                        <option value="colote">Colete</option>
-                        <option value="top">Top</option>
-                    </optgroup>
-                    <optgroup label="Vestuário Inferior">
-                        <option value="calca">Calça</option>
-                        <option value="short">Short</option>
-                        <option value="saia">Saia</option>
-                        <option value="bermuda">Bermuda</option>
-                        <option value="legging">Legging</option>
-                    </optgroup>
-                    <optgroup label="itens Para Cabeça">
-                        <option value="bone">Boné</option>
-                        <option value="chapeu">Chapéu </option>
-                        <option value="touca">Touca</option>
-                        <option value="heahband">Headband</option>
-                        <option value="lenco">Lenço</option>
-                    </optgroup>
-                    <optgroup label="Bolsas">
-                        <option value="mochila">Mochila</option>
-                        <option value="tote_bag">Tote Bag</option>
-                        <option value="sling_bag">Sling Bag</option>
-                    </optgroup>
-                    <optgroup label="Acessórios">
-                        <option value="colar">Colar</option>
-                        <option value="anel">Anél</option>
-                        <option value="pulseira">Pulseira</option>
-                        <option value="brinco">Brinco</option>
-                        <option value="pingente">Pingente</option>
-                    </optgroup>
-                    <option value="tenis">Tênis</option>
-                    <br>
-                </select>
-                <select id="selecaoDeFormato" name="selecaoDeFormato">
-                    <option selected="nullFormato">Formato</option>
-                    <option value="justo">Justo</option>
-                    <option value="normal">Normal</option>
-                    <option value="largo">Largo</option>
-                </select>
-                <br>
-                <div>
-                    <center><label style="color: #ffffff; font-size: 15px; margin-bottom: 5%; text-align: center;">
-                            Selecione a cor primária da peça:</label>
-                        <input type="color" class="form-control form-control-color" id="exampleColorInput"
-                            value="#563d7c">
-                    </center>
-                </div>
-
-                <div class="mb-3" style="margin-top: 10%;">
-                    <input class="form-control form-control-sm input_file_popUp" id="formFileSm" type="file">
-                </div>
-
-                <div style="display: flex; justify-content: flex-start; margin-top: 5%;">
-                    <button type="button" onclick="hidePopup()">Voltar</button>
-                    <button type="submit">Adicionar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-
+        <!-- div que porta todo o grid-->
+        <div class="grid-container">
             <?php
-require_once 'conectaBanco.php';
-try {
-	$sql = "SELECT * FROM imagem";
-	$stmt = $pdo->query($sql)->fetchAll();
-?>
-<?php
-	foreach ($stmt as $row) {
-?><img src=" <?php echo $row['imagem'] ?>"> <?php }}catch (PDOException $e) {
-	
-} ?>
+            require_once 'conectaBanco.php';
+            try {
+                $sql = "SELECT * FROM imagem";
+                $stmt = $pdo->query($sql)->fetchAll();
+                ?>
+                <?php
+                foreach ($stmt as $row) {
+                    ?><img src=" <?php echo $row['imagem'] ?>"> <?php }
+            } catch (PDOException $e) {
 
+            } ?>
         </div>
-    </div>
 
-    <!--! botão add peça -->
-    <div class="circle" onclick="showPopup()">
-        <div class="cross">
+        <!--! botão add peça -->
+        <div class="circle" onclick="showPopup()">
+            <div class="cross">
+            </div>
         </div>
-    </div>
 
-    <!--! pop up add peça -->
-    <div id="popup" class="popup">
-        <div class="popup-content">
-            <form action="cadastroP.php" method="post" enctype="multipart/form-data">
-
-
-                <select id="categoria" name="categoria">
-                    <option selected="nullCategoria">Categoria</option>
-                    <optgroup label="Vestuário Superior">
-
-
-                        <option value="camisa">Camiseta</option>
-                        <option value="moletom">Moletom</option>
-                        <option value="jaqueta">Jaqueta</option>
-                        <option value="regata">Regata</option>
-                        <option value="manga_longa">Camisa de Manga Longa</option>
-                        <option value="sueter">Suéter</option>
-                        <option value="colote">Colete</option>
-                        <option value="top">Top</option>
-
-
-                    </optgroup>
-                    <optgroup label="Vestuário Inferior">
-
-
-                        <option value="calca">Calça</option>
-                        <option value="short">Short</option>
-                        <option value="saia">Saia</option>
-                        <option value="bermuda">Bermuda</option>
-                        <option value="legging">Legging</option>
-
-
-                    </optgroup>
-                    <optgroup label="itens Para Cabeça">
-
-
-                        <option value="bone">Boné</option>
-                        <option value="chapeu">Chapéu </option>
-                        <option value="touca">Touca</option>
-                        <option value="heahband">Headband</option>
-                        <option value="lenco">Lenço</option>
-
-
-                    </optgroup>
-                    <optgroup label="Bolsas">
-
-
-                        <option value="mochila">Mochila</option>
-                        <option value="tote_bag">Tote Bag</option>
-                        <option value="sling_bag">Sling Bag</option>
-
-
-                    </optgroup>
-                    <optgroup label="Acessórios">
-
-
-                        <option value="colar">Colar</option>
-                        <option value="anel">Anél</option>
-                        <option value="pulseira">Pulseira</option>
-                        <option value="brinco">Brinco</option>
-                        <option value="pingente">Pingente</option>
-
-
-                    </optgroup>
-                    <option value="tenis">Tênis</option>
+        <!--! pop up add peça -->
+        <div id="popup" class="popup">
+            <div class="popup-content">
+                <form>
+                    <select id="selecaoDeCategoria" name="selecaoDeCategoria">
+                        <option selected="nullCategoria">Categoria</option>
+                        <optgroup label="Vestuário Superior">
+                            <option value="camisa">Camiseta</option>
+                            <option value="moletom">Moletom</option>
+                            <option value="jaqueta">Jaqueta</option>
+                            <option value="regata">Regata</option>
+                            <option value="manga_longa">Camisa de Manga Longa</option>
+                            <option value="sueter">Suéter</option>
+                            <option value="colote">Colete</option>
+                            <option value="top">Top</option>
+                        </optgroup>
+                        <optgroup label="Vestuário Inferior">
+                            <option value="calca">Calça</option>
+                            <option value="short">Short</option>
+                            <option value="saia">Saia</option>
+                            <option value="bermuda">Bermuda</option>
+                            <option value="legging">Legging</option>
+                        </optgroup>
+                        <optgroup label="itens Para Cabeça">
+                            <option value="bone">Boné</option>
+                            <option value="chapeu">Chapéu </option>
+                            <option value="touca">Touca</option>
+                            <option value="heahband">Headband</option>
+                            <option value="lenco">Lenço</option>
+                        </optgroup>
+                        <optgroup label="Bolsas">
+                            <option value="mochila">Mochila</option>
+                            <option value="tote_bag">Tote Bag</option>
+                            <option value="sling_bag">Sling Bag</option>
+                        </optgroup>
+                        <optgroup label="Acessórios">
+                            <option value="colar">Colar</option>
+                            <option value="anel">Anél</option>
+                            <option value="pulseira">Pulseira</option>
+                            <option value="brinco">Brinco</option>
+                            <option value="pingente">Pingente</option>
+                        </optgroup>
+                        <option value="tenis">Tênis</option>
+                        <br>
+                    </select>
+                    <select id="selecaoDeFormato" name="selecaoDeFormato">
+                        <option selected="nullFormato">Formato</option>
+                        <option value="justo">Justo</option>
+                        <option value="normal">Normal</option>
+                        <option value="largo">Largo</option>
+                    </select>
                     <br>
-                </select>
+                    <div>
+                        <center><label style="color: #ffffff; font-size: 15px; margin-bottom: 5%; text-align: center;">
+                                Selecione a cor primária da peça:</label>
+                            <input type="color" class="form-control form-control-color" id="exampleColorInput"
+                                value="#563d7c">
+                        </center>
+                    </div>
 
+                    <div class="mb-3" style="margin-top: 10%;">
+                        <input class="form-control form-control-sm input_file_popUp" id="formFileSm" type="file">
+                    </div>
 
-                <select id="formato" name="formato">
-                    <option selected="nullFormato">Formato</option>
-                    <option value="justo">Justo</option>
-                    <option value="normal">Normal</option>
-                    <option value="largo">Largo</option>
-                </select>
-                <br>
-                <div>
-                    <center><label style="color: #ffffff; font-size: 15px; margin-bottom: 5%; text-align: center;">
-                            Selecione a cor primária da peça:</label>
-
-
-                        <input type="color" class="form-control form-control-color" id="cor" name="cor" 
-                            value="#563d7c">
-                    </center>
-                </div>
-
-                <div class="mb-3" style="margin-top: 10%;">
-
-
-                    <input class="form-control form-control-sm input_file_popUp" name="fileToUpload" id="fileToUpload" type="file">
-                </div>
-
-                <div style="display: flex; justify-content: flex-start; margin-top: 5%;">
-                    <button type="button" onclick="hidePopup()">Voltar</button>
-                    <button type="submit">Adicionar</button>
-                </div>
-            </form>
+                    <div style="display: flex; justify-content: flex-start; margin-top: 5%;">
+                        <button type="button" onclick="hidePopup()">Voltar</button>
+                        <button type="submit">Adicionar</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
 
 </body>
 
